cdb999e70831b5752e92dcbbc48fccb3b9fad802
muse_stargazer <bertadams00@gmail.com>
2022-11-04 02:03:32 +0000

    adding card tracker

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
 //JS for Memory Card Game: TP8
 
+//set up card tracker
+cardsClicked = 0;
+
+function cardClicked(what) {
+  if (what.classList.contains("clicked")) {
+    //it's already clicked, act appropriately
+    what.classList.remove("clicked");
+
+  } else {
+    //it's not already clicked
+    what.classList.add("clicked");
+  }
+}
+
+
+//stuff to do when window loads
 window.onload = function() {
-  
+
   cardList = document.getElementsByClassName("card"); //collection of cards
-  
+
   cardCount = cardList.length; //how many cards are on the table
-  
+
   for (c = 0; c < cardCount; c++) {
     cardList[c].onclick = function() {
-      this.classList.toggle("clicked");
+      cardClicked(this)
     }
   }
-  
-  
+
+
 }
