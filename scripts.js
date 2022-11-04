8dad50451ecb96aeb06d639883f9bc42a52bc1b6
muse_stargazer <bertadams00@gmail.com>
2022-11-04 01:37:06 +0000

    added clicked JS

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 //JS for Memory Card Game: TP8
 
+window.onload = function() {
+  
+  cardList = document.getElementsByClassName("card"); //collection of cards
+  
+  cardCount = cardList.length; //how many cards are on the table
+  
+  for (c = 0; c < cardCount; c++) {
+    cardList[c].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+  
+  
+}
